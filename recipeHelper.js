b9df52e2b21c488b290ab893b99b022de943113a
jb0001994 <jb0001994@aol.com>
2016-11-06 14:04:02 -0800

    moved data to correct file

--- recipeHelper.js
+++ recipeHelper.js
@@ -1,54 +1,130 @@
+// This code sample shows how to call and receive external rest service data, within your skill Lambda code.
+
+// var AWS = require('aws-sdk');
+
+var https = require('https');
+
+var recipes = {
+    cookies: {
+        ingredients: {
+            milk: "one cup of",
+            butter: "all of the",
+            eggs: "one"
+        }
+    },
+    secretSauce: {
+        ingredients: {
+            butter: "two tablespoons of",
+            brownSugar: "two tablespoons of",
+            garlicCloves: "two",
+            lemonJuice: "one tablespoon of",
+            soySauce: "two teaspoons of",
+            pepper: "one half teaspoon",
+            mincedGinger: "one half teaspoon"
+        }
+    }
+}
 
 exports.handler = function( event, context ) {
     var say = "";
     var shouldEndSession = false;
     var sessionAttributes = {};
-    var myRecipe = "";
+    var myState = "";
+    var pop = 0;
+    var rank = 0;
+    var currentRecipe = "";
+    var currentIngredient = "";
+
+    if (event.session.attributes) {
+        sessionAttributes = event.session.attributes;
+    }
 
     if (event.request.type === "LaunchRequest") {
-        say = "Welcome to Recipe builder...lets get going big fella";
+        say = "Hi, what would you like to make today?";
         context.succeed({sessionAttributes: sessionAttributes, response: buildSpeechletResponse(say, shouldEndSession) });
 
     } else {
         var IntentName = event.request.intent.name;
 
-        if (IntentName === "GetRecipeRequestIntent") {
+        if (IntentName === "GetRecipeIntent") {
+            // TODO
+            // do I have a recipe for {recipe}
+            if (event.request.intent.slots.recpie.value) {
+                currentRecipe = event.request.intent.slots.recpie.value;
 
-            if (event.request.intent.slots.recipe.value) {
+                var post_data = {"recipe" : currentRecipe};
+                say = currentRecipe;
 
-                myRecipe = event.request.intent.slots.recipe.value;
-
-                say = "Damn right you have a recipe for " + myRecipe;
-
-
-                if (event.request.intent.slots.recpie.value) {
-                    currentRecipe = event.request.intent.slots.recpie.value;
-
-                    var post_data = {"recipe" : currentRecipe};
-                    // say = currentRecipe;
-
-                }
-                console.log("GetRecipeIntent");
-
-            } else if (IntentName === "GetIngredientIntent") {
-                // TODO
-                console.log("GetIngredientIntent");
-            } else if (IntentName === "GetIngredientAmmountIntent") {
-                // TODO
-                console.log("GetIngredientAmmountIntent");
-            }else if (IntentName === "GetListOfRecipesIntent") {
-                // TODO
-                console.log("GetListOfRecipesIntent");
             }
+            console.log("GetRecipeIntent");
+        } else if (IntentName === "GetIngredientIntent") {
+            // TODO
+            // "do "
+            console.log("GetIngredientIntent");
+        } else if (IntentName === "GetIngredientAmmountIntent") {
+            // TODO
+            console.log("GetIngredientAmmountIntent");
+        }else if (IntentName === "GetListOfRecipesIntent") {
+            // TODO
+            console.log("GetListOfRecipesIntent");
+        }
+
+        // if (IntentName === "StateRequestIntent") {
+        //
+        //     if (event.request.intent.slots.usstate.value) {
+        //
+        //         myState = event.request.intent.slots.usstate.value;
+        //
+        //
+        //         // call external rest service over https post
+        //         var post_data = {"usstate": myState};
+        //
+        //         var post_options = {
+        //             host:  'rmwum5l4zc.execute-api.us-east-1.amazonaws.com',
+        //             port: '443',
+        //             path: '/prod/stateresource',
+        //             method: 'POST',
+        //             headers: {
+        //                 'Content-Type': 'application/json',
+        //                 'Content-Length': Buffer.byteLength(JSON.stringify(post_data))
+        //             }};
+        //             var post_req = https.request(post_options, function(res) {
+        //                 res.setEncoding('utf8');
+        //                 var returnData = "";
+        //                 res.on('data', function (chunk) {
+        //                     returnData += chunk;
+        //                 });
+        //                 res.on('end', function () {
+        //                     // returnData: {"usstate":"Delaware","attributes":[{"population":900000},{"rank":45}]}
+        //
+        //                     pop = JSON.parse(returnData).attributes[0].population;
+        //
+        //                     say = "The population of " + myState + " is " + pop;
+        //
+        //                     // add the state to a session.attributes array
+        //                     if (!sessionAttributes.requestList) {
+        //                         sessionAttributes.requestList = [];
+        //                     }
+        //                     sessionAttributes.requestList.push(myState);
+        //
+        //                     // This line concludes the lambda call.  Move this line to within any asynchronous callbacks that return and use data.
+        //                     context.succeed({sessionAttributes: sessionAttributes, response: buildSpeechletResponse(say, shouldEndSession) });
+        //
+        //                 });
+        //               });
+        //         post_req.write(JSON.stringify(post_data));
+        //         post_req.end();
+        //
+        //     }
 
         } else if (IntentName === "AMAZON.StopIntent" || IntentName === "AMAZON.CancelIntent") {
-            say = "Happy cooking";
+            say = "You asked for " + sessionAttributes.requestList.toString() + ". Thanks for playing!";
             shouldEndSession = true;
             context.succeed({sessionAttributes: sessionAttributes, response: buildSpeechletResponse(say, shouldEndSession) });
 
 
         } else if (IntentName === "AMAZON.HelpIntent" ) {
-            say = "Ask me if you have a recipe for something"
+            say = "Just say the name of a U.S. State, such as Massachusetts or California."
             context.succeed({sessionAttributes: sessionAttributes, response: buildSpeechletResponse(say, shouldEndSession) });
 
         }
@@ -75,10 +151,3 @@ function buildSpeechletResponse(say, shouldEndSession) {
         shouldEndSession: shouldEndSession
     };
 }
-<<<<<<< HEAD
-
-
-
-
-=======
->>>>>>> d4271bef7e9c5d30b2141572e18c8bbc5638249a
