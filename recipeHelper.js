4afc786ff007b837c648217669765de6d332a805
jb0001994 <jb0001994@aol.com>
2016-11-06 18:26:08 -0800

    GetIngredientAmount is functional

--- recipeHelper.js
+++ recipeHelper.js
@@ -4,27 +4,6 @@
 
 var https = require('https');
 
-var recipes = {
-    cookies: {
-        ingredients: {
-            milk: "one cup of",
-            butter: "all of the",
-            eggs: "one"
-        }
-    },
-    secretsauce: {
-        ingredients: [
-            {name: "butter", ammount: "two tablespoons of"},
-            {name: "brownSugar", ammount: "two tablespoons of"},
-            {name: "Garlic Cloves", ammount: "two"},
-            {name: "Lemon Juice", ammount: "one tablespoon of"},
-            {name: "Soy Sauce", ammount: "two teaspoons of"},
-            {name: "pepper", ammount: "one half teaspoon"},
-            {name: "mincedGinger", ammount: "one half teaspoon"}
-        ]
-    }
-}
-
 exports.handler = function( event, context ) {
     var say = "";
     var shouldEndSession = false;
@@ -34,6 +13,29 @@ exports.handler = function( event, context ) {
     var rank = 0;
     var currentRecipe = "";
     var currentIngredient = "";
+
+    var recipes = {
+        cookies: {
+            name: "cookies",
+            ingredients: [
+                {name: "milk", ammount: "one cup of"},
+                {name: "butter", ammount: "all of the"},
+                {name: "eggs", ammount: "two"}
+            ]
+        },
+        secretsauce: {
+            name: "secret sauce",
+            ingredients: [
+                {name: "butter", ammount: "two tablespoons of"},
+                {name: "brownSugar", ammount: "two tablespoons of"},
+                {name: "Garlic Cloves", ammount: "two"},
+                {name: "Lemon Juice", ammount: "one tablespoon of"},
+                {name: "Soy Sauce", ammount: "two teaspoons of"},
+                {name: "pepper", ammount: "one half teaspoon"},
+                {name: "mincedGinger", ammount: "one half teaspoon"}
+            ]
+        }
+    };
 
     if (event.session.attributes) {
         sessionAttributes = event.session.attributes;
@@ -46,7 +48,7 @@ exports.handler = function( event, context ) {
     } else {
         var IntentName = event.request.intent.name;
 
-        if (IntentName === "GetRecipeIntent") {
+        if (IntentName === "CheckRecipeIntent") {
             // TODO
             // do I have a recipe for {recipe}
             if (event.request.intent.slots.recpie.value) {
@@ -55,69 +57,44 @@ exports.handler = function( event, context ) {
                 var post_data = {"recipe" : currentRecipe};
                 say = currentRecipe;
 
+
             }
             console.log("GetRecipeIntent");
+            context.succeed({sessionAttributes: sessionAttributes, response: buildSpeechletResponse(say, shouldEndSession) });
         } else if (IntentName === "GetIngredientIntent") {
             // TODO
-            // "do "
             console.log("GetIngredientIntent");
-        } else if (IntentName === "GetIngredientAmmountIntent") {
+            context.succeed({sessionAttributes: sessionAttributes, response: buildSpeechletResponse(say, shouldEndSession) });
+
+        } else if (IntentName === "GetIngredientAmountIntent") {
+            var currentRecipe = event.request.intent.slots.recipe.value;
+            var currentIngredient = event.request.intent.slots.ingredient.value;
             // TODO
-            currentIngredient = event.request.intent.slots.;
+            if (event.request.intent.slots.recipe.value) {
+                console.log(currentRecipe);
+                console.log(currentIngredient);
+                recipe = currentRecipe.replace(/ /g, '');
+                var ammount;
+                var ingredientList = recipes[recipe].ingredients;
+                for (var i = 0; i < ingredientList.length; i += 1) {
+                    if (ingredientList[i].name === currentIngredient) {
+                        ammount = ingredientList[i].ammount;
+                    }
+                }
+                say = "You need " + ammount + " " + currentIngredient;
+                shouldEndSession = true;
+            } else {
+                console.log("FUCK");
+                say = "Sorry, I can't find that ingredient";
+            }
+
             console.log("GetIngredientAmmountIntent");
-        }else if (IntentName === "GetListOfRecipesIntent") {
+            context.succeed({sessionAttributes: sessionAttributes, response: buildSpeechletResponse(say, shouldEndSession) });
+
+        } else if (IntentName === "GetListOfRecipesIntent") {
             // TODO
             console.log("GetListOfRecipesIntent");
-        }
-
-        // if (IntentName === "StateRequestIntent") {
-        //
-        //     if (event.request.intent.slots.usstate.value) {
-        //
-        //         myState = event.request.intent.slots.usstate.value;
-        //
-        //
-        //         // call external rest service over https post
-        //         var post_data = {"usstate": myState};
-        //
-        //         var post_options = {
-        //             host:  'rmwum5l4zc.execute-api.us-east-1.amazonaws.com',
-        //             port: '443',
-        //             path: '/prod/stateresource',
-        //             method: 'POST',
-        //             headers: {
-        //                 'Content-Type': 'application/json',
-        //                 'Content-Length': Buffer.byteLength(JSON.stringify(post_data))
-        //             }};
-        //             var post_req = https.request(post_options, function(res) {
-        //                 res.setEncoding('utf8');
-        //                 var returnData = "";
-        //                 res.on('data', function (chunk) {
-        //                     returnData += chunk;
-        //                 });
-        //                 res.on('end', function () {
-        //                     // returnData: {"usstate":"Delaware","attributes":[{"population":900000},{"rank":45}]}
-        //
-        //                     pop = JSON.parse(returnData).attributes[0].population;
-        //
-        //                     say = "The population of " + myState + " is " + pop;
-        //
-        //                     // add the state to a session.attributes array
-        //                     if (!sessionAttributes.requestList) {
-        //                         sessionAttributes.requestList = [];
-        //                     }
-        //                     sessionAttributes.requestList.push(myState);
-        //
-        //                     // This line concludes the lambda call.  Move this line to within any asynchronous callbacks that return and use data.
-        //                     context.succeed({sessionAttributes: sessionAttributes, response: buildSpeechletResponse(say, shouldEndSession) });
-        //
-        //                 });
-        //               });
-        //         post_req.write(JSON.stringify(post_data));
-        //         post_req.end();
-        //
-        //     }
-
+            context.succeed({sessionAttributes: sessionAttributes, response: buildSpeechletResponse(say, shouldEndSession) });
         } else if (IntentName === "AMAZON.StopIntent" || IntentName === "AMAZON.CancelIntent") {
             say = "You asked for " + sessionAttributes.requestList.toString() + ". Thanks for playing!";
             shouldEndSession = true;
